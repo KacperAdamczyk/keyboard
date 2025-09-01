9bc7d30887a41324ac6e3920cf301d79174a1a2e
Kacper Adamczyk <kacper@admck.com>
2025-09-01 16:59:43 +0200

    Format

--- app.config.ts
+++ app.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-    server: {
-        experimental: {
-            websocket: true,
-        },
-    },
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	server: {
+		experimental: {
+			websocket: true,
+		},
+	},
 }).addRouter({
-    name: "ws",
-    type: "http",
-    handler: "./src/ws.ts",
-    target: "server",
-    base: "/ws",
+	name: "ws",
+	type: "http",
+	handler: "./src/ws.ts",
+	target: "server",
+	base: "/ws",
 });
--- src/ws.ts
+++ src/ws.ts
@@ -1,19 +1,19 @@
+import r from "robotjs";
 import { eventHandler } from "vinxi/http";
-import r from 'robotjs'
 
 export default eventHandler({
-    handler() {},
-    websocket: {
-        open(peer) {
-            console.log('Connected', peer.id);
-        },
-        async message(peer, msg) {
-            const message = msg.text();
-            console.log('Clicking:', message);
-            r.keyTap(message)
-        },
-close(peer) {
-            console.log('Disconnected', peer.id);
-}
-    },
+	handler() {},
+	websocket: {
+		open(peer) {
+			console.log("Connected", peer.id);
+		},
+		async message(peer, msg) {
+			const message = msg.text();
+			console.log("Clicking:", message);
+			r.keyTap(message);
+		},
+		close(peer) {
+			console.log("Disconnected", peer.id);
+		},
+	},
 });
